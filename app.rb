410643df1f708073f538a5126e1f87044f59a558
Karen Flores <karen.ag.flores@gmail.com>
2019-10-29 22:18:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirate' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ships.new(details)
+      end
+
+      erb :pirate
+    end
   end
 end
